e06a369127030aadf0d638c203c8f3e4a97dd8cd
Spencer Williams <spencerwi@gmail.com>
2021-09-22 12:39:54 +0000

    Add lualine theme port

---
+++ lua/lualine/themes/nisha.lua
@@ -0,0 +1,27 @@
+return {
+    normal = {
+        a = {fg = '#191a21', bg = '#ff7d55'},
+        b = {fg = '#191a21', bg = '#99b7c2'},
+        c = {fg = '#efe6dd', bg = '#373a42'},
+    },
+    insert = {
+        a = {fg = '#191a21', bg = '#6bc29a'},
+        b = {fg = '#191a21', bg = '#99b7c2'},
+        c = {fg = '#efe6dd', bg = '#373a42'},
+    },
+    replace = {
+        a = {fg = '#191a21', bg = '#f14358'},
+        b = {fg = '#191a21', bg = '#99b7c2'},
+        c = {fg = '#efe6dd', bg = '#373a42'},
+    },
+    visual = {
+        a = {fg = '#191a21', bg = '#ab797a'},
+        b = {fg = '#191a21', bg = '#99b7c2'},
+        c = {fg = '#efe6dd', bg = '#373a42'},
+    },
+    inactive = {
+        a = {fg = '#efe6dd', bg = '#191a21'},
+        b = {fg = '#191a21', bg = '#99b7c2'},
+        c = {fg = '#efe6dd', bg = '#373a42'},
+    },
+}
